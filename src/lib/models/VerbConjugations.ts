9b1095439084ca468b1a544563db83e1d0ea5368
eg-heiti-christian <christianflickinger94@gmail.com>
2024-10-01 19:19:50 -0400

    Swapping nextjs with vite

--- src/lib/models/VerbConjugations.ts
+++
@@ -1,9 +0,0 @@
-
-export interface VerbConjugations {
-    singularFirstPerson: string;
-    singularSecondPerson: string;
-    singularThirdPerson: string;
-    pluralFirstPerson: string;
-    pluralSecondPerson: string;
-    pluralThirdPerson: string;
-}
