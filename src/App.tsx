8af610937b3b6b60b47f6dd5be3925567dd6210f
eg-heiti-christian <christianflickinger94@gmail.com>
2024-10-21 18:09:38 -0400

    Installing RQ Devtools

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { Router } from './Router';
 import { theme, resolver } from './theme';
@@ -15,6 +16,7 @@ export default function App() {
     <MantineProvider forceColorScheme='dark' theme={theme} cssVariablesResolver={resolver}>
       <QueryClientProvider client={queryClient}>
         <Router />
+        <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
     </MantineProvider>
   );
